626f4a9162aefa053822dcd6577162823e3cc4ae
Fabrice Beya <byxfab001@myuct.ac.za>
2019-05-11 01:22:33 +0200

    Implemented basic user search

--- screens/Search.js
+++ screens/Search.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Content, Text, List, Item, ListItem, Input, H1, View, H2, H3, Icon, Separator, Container, Left, Right, Badge, Footer, Button, Thumbnail, Body, Image } from "native-base";
 import styles from '../styles';
+import db from '../config/firebase';
 
 class Search extends React.Component {
+
+  state = {
+    search: '',
+    results: []
+  }
+
+  searchUser = async () => {
+    let results = []
+    const query = await db.collection('users').where('fullname', '>=', this.state.search).get()
+    query.forEach((response) => {
+      results.push(response.data())
+    })
+    this.setState({results: results})
+    console.log(results)
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Search</Text>
-      </View>
+      <Container>
+        <Content>
+        <View style={{flex:1}}>
+          <Item regular>
+            <Input
+              value={this.state.search}
+              placeholder='search'
+              autoFocus={true}
+              returnKeyType='search'
+              onSubmitEditing={this.searchUser}
+              onChangeText={search => this.setState({search})} />
+          </Item>
+          <List
+            dataArray={this.state.results}
+            renderRow={(item) =>
+              <ListItem thumbnail  onPress={() => this.props.navigation.navigate('Profile')} >
+                <Left style={{flexDirection: 'column', alignItems: 'center'}}>
+                    <Thumbnail style={{borderRadius: 2}} large square source={{ uri: item.photo }} />
+                    <Text note>{item.fullname}</Text>
+                </Left>
+                <Body>
+                  <Text>{item.email}</Text>     
+                  <Text>{item.residence} - {item.unit}</Text> 
+                </Body>
+              </ListItem>}
+            />
+          </View>
+          </Content>
+      </Container>  
+      
     );
   }
 }
